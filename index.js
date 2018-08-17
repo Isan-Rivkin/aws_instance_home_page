f0eabb07b7f9d542acc034d390be9951a8239770
Isan-Rivkin <isanrivkin@gmail.com>
2018-08-18 02:15:41 +0300

    https upgrade

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
+
 const path = require('path');
-var serveStatic = require('serve-static')
 const express = require('express');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(serveStatic(path.join(__dirname, 'public'), {'index': ['my_page.html']}));
+
+// app.get('/', (req, res) => {
+//     let file = path.join(__dirname,'public/my_page.html');
+//     res.sendFile(file);
+// });
+
+// app.get('/.well-known/acme-challenge/Q29yPuzyrG8toy0FKSmGRFAaUOwUM3PDRil4ptg0UAw', (req,res)=> {
+// 	let cert = path.join(__dirname,'public/.well-known/acme-challenge/Q29yPuzyrG8toy0FKSmGRFAaUOwUM3PDRil4ptg0UAw');
+// 	res.sendFile(cert);
+// });
+
+// app.get('/.well-known/acme-challenge/mNvuGsW1_Fd42_LxZTsYzW-mFROb0b8kv2-8toH485M', (req,res)=> {
+//         let cert = path.join(__dirname,'public/.well-known/acme-challenge/mNvuGsW1_Fd42_LxZTsYzW-mFROb0b8kv2-8toH485M');
+//         res.sendFile(cert);
+// });
+// app.listen(80, () => console.log('Example app listening on port 3000!'));
+
+///
+
+
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/isan.cf/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/isan.cf/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/isan.cf/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
 
 app.get('/', (req, res) => {
     let file = path.join(__dirname,'public/my_page.html');
@@ -20,4 +49,14 @@ app.get('/.well-known/acme-challenge/mNvuGsW1_Fd42_LxZTsYzW-mFROb0b8kv2-8toH485M
         let cert = path.join(__dirname,'public/.well-known/acme-challenge/mNvuGsW1_Fd42_LxZTsYzW-mFROb0b8kv2-8toH485M');
         res.sendFile(cert);
 });
-app.listen(80, () => console.log('Example app listening on port 3000!'));
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
