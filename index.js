8b7e8623975530cb8d1ecebaf3893e7050cb9281
Isan-Rivkin <isanrivkin@gmail.com>
2018-08-18 01:29:07 +0300

    initial page

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const express = require('express')
-const app = express()
+const path = require('path');
+var serveStatic = require('serve-static')
+const express = require('express');
+const app = express();
 
-app.get('/', (req, res) => res.send('Nuni I Love you gozal katan!!'))
+app.use(express.static(path.join(__dirname, 'public')));
+//app.use(serveStatic(path.join(__dirname, 'public'), {'index': ['my_page.html']}));
 
-app.listen(80, () => console.log('Example app listening on port 80!'))
+app.get('/', (req, res) => {
+    let file = path.join(__dirname,'public/my_page.html');
+    res.sendFile(file);
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
