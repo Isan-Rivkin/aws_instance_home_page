fd2fadbc0a503caab9c206d52063ead28c110364
Ubuntu <ubuntu@ip-172-31-37-173.eu-west-3.compute.internal>
2018-08-20 16:43:44 +0000

    google analytics

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
 
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const https = require('https');
 const app = express();
-
+const fs = require('fs');
 app.use(express.static(path.join(__dirname, 'public')));
-
-// app.get('/', (req, res) => {
-//     let file = path.join(__dirname,'public/my_page.html');
-//     res.sendFile(file);
-// });
-
-// app.get('/.well-known/acme-challenge/Q29yPuzyrG8toy0FKSmGRFAaUOwUM3PDRil4ptg0UAw', (req,res)=> {
-// 	let cert = path.join(__dirname,'public/.well-known/acme-challenge/Q29yPuzyrG8toy0FKSmGRFAaUOwUM3PDRil4ptg0UAw');
-// 	res.sendFile(cert);
-// });
-
-// app.get('/.well-known/acme-challenge/mNvuGsW1_Fd42_LxZTsYzW-mFROb0b8kv2-8toH485M', (req,res)=> {
-//         let cert = path.join(__dirname,'public/.well-known/acme-challenge/mNvuGsW1_Fd42_LxZTsYzW-mFROb0b8kv2-8toH485M');
-//         res.sendFile(cert);
-// });
-// app.listen(80, () => console.log('Example app listening on port 3000!'));
-
-///
-
 
 // Certificate
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/isan.cf/privkey.pem', 'utf8');
