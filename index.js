b09b5256af7dfd9f7d02509d92bdf071ce6be0ca
Isan-Rivkin <isanrivkin@gmail.com>
2019-03-31 18:20:27 +0300

    FIX: added iluzis

--- index.js
+++ index.js
@@ -32,6 +32,122 @@ app.get('/.well-known/acme-challenge/mNvuGsW1_Fd42_LxZTsYzW-mFROb0b8kv2-8toH485M
         let cert = path.join(__dirname,'public/.well-known/acme-challenge/mNvuGsW1_Fd42_LxZTsYzW-mFROb0b8kv2-8toH485M');
         res.sendFile(cert);
 });
+const getRand = (list)=>{
+    return list[Math.floor(Math.random()*list.length)];
+}
+const iluz_is = ()=> {
+    let appearance = [
+        "attractive",
+        "bald",
+        "beautiful",
+        "chubby",
+        "clean",
+        "dazzling",
+        "drab",
+        "elegant",
+        "fancy",
+        "fit",
+        "flabby",
+        "glamorous",
+        "gorgeous",
+        "handsome",
+        "long",
+        "magnificent",
+        "muscular",
+        "plain",
+        "plump",
+        "quaint",
+        "scruffy",
+        "shapely",
+        "short",
+        "skinny",
+        "stocky",
+        "ugly",
+        "unkempt",
+        "unsightly",
+    ];
+    let positivePersonality = [
+        "aggressive",
+        "agreeable",
+        "ambitious",
+        "brave",
+        "calm",
+        "delightful",
+        "eager",
+        "faithful",
+        "gentle",
+        "happy",
+        "jolly",
+        "kind",
+        "lively",
+        "nice",
+        "obedient",
+        "polite",
+        "proud",
+        "silly",
+        "thankful",
+        "victorious",
+        "witty",
+        "wonderful",
+        "zealous",
+    ];
+    let negativePersonality = [
+        "angry",
+        "bewildered",
+        "clumsy",
+        "defeated",
+        "embarrassed",
+        "fierce",
+        "grumpy",
+        "helpless",
+        "itchy",
+        "jealous",
+        "lazy",
+        "mysterious",
+        "nervous",
+        "obnoxious",
+        "panicky",
+        "pitiful",
+        "repulsive",
+        "scary",
+        "thoughtless",
+        "uptight",
+        "worried",
+    ];    
+    let size = [
+        "big",
+        "colossal",
+        "fat",
+        "gigantic",
+        "great",
+        "huge",
+        "immense",
+        "large",
+        "little",
+        "mammoth",
+        "massive",
+        "microscopic",
+        "miniature",
+        "petite",
+        "puny",
+        "scrawny",
+        "short",
+        "small",
+        "tall",
+        "teeny",
+        "tiny",
+    ];
+    // story 
+    let posWord = getRand(positivePersonality) 
+    let negWord = getRand(negativePersonality)
+    let sizeWord = getRand(size)
+    let appWord = getRand(appearance)
+    let text = `Iluz is so ... ${posWord}, too bad because he is also very ${negWord}, his ${sizeWord} brain is very ${appWord}`
+    return text
+}
+app.get('/iluz_is',(req,res) => {
+    res.end(iluz_is())
+});
 // Starting both http & https servers
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer(credentials, app);
